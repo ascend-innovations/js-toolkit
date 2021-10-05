bd99035ea7110608a985bb09ba3b47bb6d5de0b6
Chris Hahn <chahn@ascend-innovations.com>
2021-10-05 16:26:20 -0400

    feat: improve styelinting

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,9 +1,18 @@
-modules.export = {
-    extends: [
-        'stylelint-config-recommended',
-    ],
+module.exports = {
+    extends: 'stylelint-config-standard',
+    plugins: ['stylelint-declaration-strict-value', 'stylelint-order'],
     rules: {
+        // cannot use arbitrary values for the defined properties.
+        "scale-unlimited/declaration-strict-value": [
+            ["/color$/", 'fill', 'font-size', '/margin$/', '/padding$/', 'stroke', 'z-index'],
+            {
+                ignoreValues: [ 'currentColor', 'inherit', 'transparent']
+            }
+        ],
+        'order/properties-alphabetical-order': true,
+        "custom-property-empty-line-before": "never",
         'color-hex-length': 'long',
+        'color-hex-case': 'upper',
         'color-named': 'never',
         'unit-allowed-list': [
             'px',

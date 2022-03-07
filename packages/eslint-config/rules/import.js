e2581cad4b1a282c91a5263e3dfbc6e2e70623b1
Chris Hahn <chahn@ascend-innovations.com>
2022-03-07 10:18:51 -0500

    chore: add svelte-specific rules

--- packages/eslint-config/rules/import.js
+++ packages/eslint-config/rules/import.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
+    // We prefer named exports across the board
     'import/prefer-default-export': 'off',
     import: 'off',
     'import/extensions': 'off',
--- packages/eslint-config/rules/svelte.js
+++ packages/eslint-config/rules/svelte.js
@@ -20,6 +20,8 @@ module.exports = {
       },
     ],
     'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
   },
   overrides: [
     {
